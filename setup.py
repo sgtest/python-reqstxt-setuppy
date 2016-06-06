4310ca12c2b18df013ecf9650097518c06e7b94a
Beyang Liu <beyang@sourcegraph.com>
2016-06-06 16:48:02 -0700

    setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='python-reqstxt-setuppy',
+    version='',
+    url='http://github.com/sgtest/python-reqstxt-setuppy',
+    py_modules=['mod123'],
+    install_requires=[
+        'Jinja2>=2.4',
+    ],
+)
